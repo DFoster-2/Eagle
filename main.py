56b80b9a1a1fd9139b9e3c48d758fe343131f57e
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2023-11-02 13:58:43 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+#from Apps.Encrypter.EncryptHomeScreen import *; encryptstart2()
+from Software.Start import *
+#from Apps.Translater.Translat import *
+#from Software.Login.Encrpt import  encrypt; encrypt()
+#from Software.Login.Decrypt import  decrypt; decrypt()
+#from Software.Login.sineIn import SineIn; SineIn("hi","moo")
+#from Apps.EncrypterFiles.main2 import *
+#from Software.Login.SineUp import SineuP; SineuP("ghjdjk","hdfhdhdfdfhdh")
